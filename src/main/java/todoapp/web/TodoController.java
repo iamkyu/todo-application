8cc5ad0588c80c4c5bf41436e88b7871c57b35f6
iamkyu <kyujnam@gmail.com>
2018-12-16 14:01:21 +0900

    refactor: 어노테이션을 통한 설정 정보 로드

--- src/main/java/todoapp/web/TodoController.java
+++ src/main/java/todoapp/web/TodoController.java
@@ -1,6 +1,6 @@
 package todoapp.web;
 
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,16 +9,13 @@ import todoapp.web.model.SiteProperties;
 @Controller
 public class TodoController {
 
-    private final Environment environment;
-
-    public TodoController(Environment environment) {
-        this.environment = environment;
-    }
+    @Value("${site.author}")
+    private String author;
 
     @GetMapping("/todos")
     public void getTodos(Model model) {
         SiteProperties site = new SiteProperties();
-        site.setAuthor(environment.getProperty("site.author"));
+        site.setAuthor(author);
         model.addAttribute("site", site);
     }
 }
