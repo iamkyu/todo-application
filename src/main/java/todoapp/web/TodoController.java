c4760ee4387861819a2ce963229097864d98a8c4
iamkyu <kyujnam@gmail.com>
2018-12-16 14:14:15 +0900

    refactor: `@ModelAttribute`를 통해 Todo 컨트롤러에서 공통으로 사용하는 속성 지정

--- src/main/java/todoapp/web/TodoController.java
+++ src/main/java/todoapp/web/TodoController.java
@@ -1,8 +1,8 @@
 package todoapp.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import todoapp.web.model.SiteProperties;
 
 @Controller
@@ -14,7 +14,11 @@ public class TodoController {
     }
 
     @GetMapping("/todos")
-    public void getTodos(Model model) {
-        model.addAttribute("site", siteProperties);
+    public void getTodos() {
+    }
+
+    @ModelAttribute("site")
+    public SiteProperties getSiteProperties() {
+        return siteProperties;
     }
 }
