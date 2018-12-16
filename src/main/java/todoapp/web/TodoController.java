973da7cd2b97241e0be68250b14a0581ef92ca68
iamkyu <kyujnam@gmail.com>
2018-12-16 14:00:23 +0900

    feat: `Environment` 개체를 통한 설정 정보 로드

--- src/main/java/todoapp/web/TodoController.java
+++ src/main/java/todoapp/web/TodoController.java
@@ -1,5 +1,6 @@
 package todoapp.web;
 
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +9,16 @@ import todoapp.web.model.SiteProperties;
 @Controller
 public class TodoController {
 
+    private final Environment environment;
+
+    public TodoController(Environment environment) {
+        this.environment = environment;
+    }
+
     @GetMapping("/todos")
     public void getTodos(Model model) {
         SiteProperties site = new SiteProperties();
+        site.setAuthor(environment.getProperty("site.author"));
         model.addAttribute("site", site);
     }
 }
