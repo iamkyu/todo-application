37f2df50771ca6f4804d7a1e5e973d7a2c4f6eb5
iamkyu <kyujnam@gmail.com>
2018-12-17 10:16:01 +0900

    feat: BindingResult를 통한 에러 코드 응답

--- src/main/java/todoapp/web/UserController.java
+++ src/main/java/todoapp/web/UserController.java
@@ -3,6 +3,8 @@ package todoapp.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +22,16 @@ public class UserController {
     }
 
     @PostMapping
-    public void doLogin(@Valid LoginCommand command) {
+    public String doLogin(@Valid LoginCommand command, BindingResult bindingResult, Model model) {
         log.debug("command: {}", command);
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("bindingResult", bindingResult);
+            model.addAttribute("message", "사용자 이름 또는 비밀번호가 올바르지 않습니다.");
+            return "login";
+        }
+
+        return "redirect:/todos";
     }
 
     private static class LoginCommand {
