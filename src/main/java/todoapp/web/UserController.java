af216826012e2b3f5cb23b736e9e46ab86df6ec0
iamkyu <kyujnam@gmail.com>
2018-12-17 10:00:51 +0900

    feat: POST /login

--- src/main/java/todoapp/web/UserController.java
+++ src/main/java/todoapp/web/UserController.java
@@ -1,27 +1,61 @@
 package todoapp.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import todoapp.web.model.SiteProperties;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Size;
 
 @Controller
 @RequestMapping(value = "/login")
 public class UserController {
-    private final SiteProperties siteProperties;
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    public UserController(SiteProperties siteProperties) {
-        this.siteProperties = siteProperties;
-    }
-
-    @GetMapping("/login")
+    @GetMapping
     public void getLoginPage() {
     }
 
+    @PostMapping
+    public void doLogin(@Valid LoginCommand command) {
+        log.debug("command: {}", command);
+    }
 
-    @ModelAttribute("site")
-    public SiteProperties getSiteProperties() {
-        return siteProperties;
+    private static class LoginCommand {
+        @Size(min = 4, max = 20)
+        private String username;
+        @Size(min = 8)
+        private String password;
+
+        private LoginCommand() {
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        // FIXME password mask
+        @Override
+        public String toString() {
+            return "LoginCommand{" +
+                    "username='" + username + '\'' +
+                    ", password='" + password + '\'' +
+                    '}';
+        }
     }
 }
